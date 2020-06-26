fbb633be6aa9b38ae9732b0d5ae9f0b87f22ae62
Hesler Ferrera <heslerferrera@gmail.com>
2020-06-26 00:24:41 -0600

    modificacion botones y movimiento entre diferentes Activity

--- proyecto1/app/src/main/java/com/negocios/proyecto1/Main2Activity.java
+++ proyecto1/app/src/main/java/com/negocios/proyecto1/Main2Activity.java
@@ -2,7 +2,9 @@ package com.negocios.proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public  void atras(View view){
+        Intent miIntent = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(miIntent);
     }
 }
